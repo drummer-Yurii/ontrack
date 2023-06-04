9fb40df4bf77ffca630a1506d09741dc806b8ab9
Yurii Rybachok <drummer2009@ukr.net>
2023-06-04 19:09:30 +0300

    clickable links in header

--- src/App.vue
+++ src/App.vue
@@ -20,10 +20,17 @@ function normalizePageHash() {
 
   return PAGE_TIMELINE
 }
+
+function goTo(page) {
+  currentPage.value = page
+}
 </script>
 
 <template>
-  <TheHeader />
+  <TheHeader 
+    @go-to-timeline="goTo(PAGE_TIMELINE)" 
+    @go-to-progress="goTo(PAGE_PROGRESS)" 
+  />
 
   <main class="flex flex-grow flex-col">
     <TheTimeline v-show="currentPage === PAGE_TIMELINE" />
@@ -31,5 +38,5 @@ function normalizePageHash() {
     <TheProgressVue v-show="currentPage === PAGE_PROGRESS" />
   </main>
 
-  <TheNav :current-page="currentPage" @navigate="currentPage = $event" />
+  <TheNav :current-page="currentPage" @navigate="goTo($event)" />
 </template>
