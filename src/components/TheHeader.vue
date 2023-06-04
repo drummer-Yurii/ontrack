9f80c4dba4724373e55f7e2b443fab60a6c4394c
Yurii Rybachok <drummer2009@ukr.net>
2023-06-04 14:47:31 +0300

    create components for header & navigation

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,11 @@
+<script setup>
+import TheHeaderProgress from './TheHeaderProgress.vue';
+import TheLogo from './TheLogo.vue';
+</script>
+
+<template>
+    <header class="sticky top-0 z-20 flex items-center justify-between border-b bg-white p-3">
+        <TheLogo />
+        <TheHeaderProgress />
+    </header>
+</template>
