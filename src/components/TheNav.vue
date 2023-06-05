1a735cdc46d59d43a69cfa3563a8ce51ab8928ba
Yurii Rybachok <drummer2009@ukr.net>
2023-06-05 20:59:42 +0300

    create reusable validator

--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -1,14 +1,13 @@
 <script setup>
 import { NAV_ITEMS } from '../constants';
 import NavItem from './NavItem.vue';
+import { isPageValid } from '../validators';
 
 defineProps({
     currentPage: {
         required: true,
         type: String,
-        validator(currentPage) {
-            return Object.keys(NAV_ITEMS).includes(currentPage)
-        }
+        validator: isPageValid
     }
 }) 
 
