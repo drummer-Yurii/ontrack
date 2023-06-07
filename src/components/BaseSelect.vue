eca717d0d9ee07cb9461960f8a5bb103f0a11833
Yurii Rybachok <drummer2009@ukr.net>
2023-06-07 12:03:33 +0300

    form for create activities

--- src/components/BaseSelect.vue
+++ src/components/BaseSelect.vue
@@ -1,8 +1,9 @@
 <script setup>
+import { computed } from 'vue';
 import { XMarkIcon } from '@heroicons/vue/24/outline';
+import { BUTTON_TYPE_NEUTRAL } from '../constants';
 import { validateSelectOptions, isUndefinedOrNull, isNumberOrNull } from '../validators';
 import BaseButton from './BaseButton.vue';
-import { computed } from 'vue';
 
 const props = defineProps({
     selected: Number,
@@ -26,7 +27,7 @@ const isNotSelected = computed(() => isUndefinedOrNull(props.selected))
 
 <template>
     <div class="flex gap-2">
-        <BaseButton @click="emit('select', null)">
+        <BaseButton :type="BUTTON_TYPE_NEUTRAL" @click="emit('select', null)">
             <XMarkIcon class="h-8" />
         </BaseButton>
         <select 
--- src/pages/TheActivities.vue
+++ src/pages/TheActivities.vue
@@ -1,6 +1,8 @@
 <script setup>
+import { PlusIcon } from '@heroicons/vue/24/outline';
 import { isActivityValid, validateActivities } from '../validators';
 import ActivityItem from '../components/ActivityItem.vue';
+import BaseButton from '../components/BaseButton.vue';
 
 
 defineProps({
@@ -26,5 +28,11 @@ const emit = defineEmits({
             @delete="emit('deleteActivity', activity)" 
         />
         </ul>
+        <form class="sticky bottom-[57px] flex gap-2 border-t bg-white p-4">
+            <input type="text" class="w-full rounded border px-4 text-xl" placeholder="Activity name">
+            <BaseButton>
+                <PlusIcon class="h-8" />
+            </BaseButton>
+        </form>
     </div>
 </template>
