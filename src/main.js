c2458cb4198a413656c84d97452f0701c3c83fb6
Yurii Rybachok <drummer2009@ukr.net>
2023-11-23 11:43:43 +0200

    refactoring storage module

--- src/main.js
+++ src/main.js
@@ -1,8 +1,5 @@
 import { createApp } from 'vue'
-import * as storage from './storage'
-import { activities } from './activities'
-import { timelineItems } from './timeline-items'
-import { isToday } from './time'
+import { loadState, saveState } from './storage'
 import App from './App.vue'
 
 import './assets/main.css'
@@ -12,19 +9,5 @@ loadState()
 document.addEventListener('visibilitychange', () => {
   document.visibilityState === 'visible' ? loadState() : saveState()
 })
-
-function loadState() {
-  const state = storage.load()
-
-  timelineItems.value = isToday(new Date(state.date)) ? state.timelineItems : timelineItems.value
-  activities.value = state.activities || activities.value
-}
-
-function saveState() {
-  storage.save({
-    timelineItems: timelineItems.value,
-    activities: activities.value
-  })
-}
 
 createApp(App).mount('#app')
