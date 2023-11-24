d5a5addf17854758a3190ec4fb3b6c211fd51046
Yurii Rybachok <drummer2009@ukr.net>
2023-11-24 10:12:27 +0200

    global timer to start the stopwatch

--- src/pages/TheTimeline.vue
+++ src/pages/TheTimeline.vue
@@ -2,8 +2,10 @@
 import { onActivated, onDeactivated } from 'vue';
 import TheTimelineIndecator from '../components/TheTimelineIndecator.vue';
 import TimelineItem from '../components/TimelineItem.vue';
-import { timelineItems, timelineItemRefs, scrollToCurrentHour } from '../timeline-items';
+import { timelineItems, timelineItemRefs, scrollToCurrentHour, stopTimelineItemTimer } from '../timeline-items';
 import { startTimer, stopTimer } from '../time'
+
+stopTimelineItemTimer()
 
 onActivated(() => {
     scrollToCurrentHour()
