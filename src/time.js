cbeca594181d4f72b3917fa23f3a9bb81fdf399d
Yurii Rybachok <drummer2009@ukr.net>
2023-11-25 10:42:02 +0200

    limiting the number of mesured seconds

--- src/time.js
+++ src/time.js
@@ -2,13 +2,12 @@ import { ref, computed } from 'vue'
 import {
   HUNDRED_PERCENT,
   MILLISECONDS_IN_SECOND,
-  SECONDS_IN_DAY,
-  SECONDS_IN_MINUTE
+  SECONDS_IN_DAY
 } from './constants'
 
 export function today() {
   const today = new Date()
-  today.setHours(0, 0)
+  // today.setHours(0, 0)
   return today
 }
 
@@ -37,7 +36,7 @@ let timer = null
 export function startTimer() {
   now.value = today()
   timer = setInterval(() => {
-    now.value = new Date(now.value.getTime() + SECONDS_IN_MINUTE * MILLISECONDS_IN_SECOND)
+    now.value = new Date(now.value.getTime() + MILLISECONDS_IN_SECOND)
   }, MILLISECONDS_IN_SECOND)
 }
 
